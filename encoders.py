5e0c302264a8121d69957ce7e541d10d48cac69d
ClutchTech <clutchshadow26@gmail.com>
2022-11-21 18:04:01 -0500

    init

--- encoders.py
+++ encoders.py
@@ -5,35 +5,47 @@ Used to quickly encode / decode strings.
 """
 
 
-class Basic:
+class EncoderDecoder:
     def __init__(self, data: str) -> None:
         self.data: str = data
+        self.altered_data: str = ''
 
         if not isinstance(data, str):
             raise TypeError("Input 'data' requires type 'str'.")
 
+    def __call__(self):
+        return {
+            "data": self.data,
+            "altered_data": self.altered_data
+        }
 
-class Base64(Basic):
-    def encode(self) -> str:
-        encoded = urlsafe_b64encode(self.data.encode('utf-8'))
-        return str(encoded, 'utf-8')
+    def base64_encode(self) -> str:
+        if self.altered_data:
+            encoded = urlsafe_b64encode(self.altered_data.encode('utf-8'))
+        else:
+            encoded = urlsafe_b64encode(self.data.encode('utf-8'))
+        self.altered_data = str(encoded, 'utf-8')
+        return self.altered_data
 
-    def decode(self) -> str:
-        decoded = urlsafe_b64decode(self.data)
-        return decoded.decode()
+    def base64_decode(self) -> str:
+        if self.altered_data:
+            decoded = urlsafe_b64decode(self.altered_data)
+        else:
+            decoded = urlsafe_b64decode(self.data)
+        self.altered_data = decoded.decode()
+        return self.altered_data
 
+    def hex_encode(self) -> str:
+        if self.altered_data:
+            bytes_conversion = self.altered_data.encode('utf-8')
+        else:
+            bytes_conversion = self.data.encode('utf-8')
+        self.altered_data = bytes_conversion.hex()
+        return self.altered_data
 
-class Hex(Basic):
-    def encode(self) -> str:
-        bytes_conversion = self.data.encode('utf-8')
-        return bytes_conversion.hex()
-
-    def decode(self) -> str:
-        return bytes.fromhex(self.data).decode('utf-8')
-
-
-if __name__ == '__main__':
-    print(
-        Base64(data="Encode this").encode(),
-        sep='\n'
-    )
+    def hex_decode(self) -> str:
+        if self.altered_data:
+            self.altered_data = bytes.fromhex(self.altered_data).decode('utf-8')
+        else:
+            self.altered_data = bytes.fromhex(self.data).decode('utf-8')
+        return self.altered_data
