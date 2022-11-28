9d6da308a8cbd66362972cd879395b3521b694f9
ClutchTech <rollout26@gmail.com>
2022-11-28 02:13:57 -0500

    save point

--- CyberBook/interactive/main.py
+++ CyberBook/interactive/main.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+from CyberBook.encoders import DecoderRing
 from CyberBook.listener import SummoningCircle
 
 
@@ -9,6 +12,16 @@ class CyberBookInterpreter:
 
         self.prompt: str = '[CBI]-$ '
         self.command_prefix: str = '--'
+
+        self.sc = SummoningCircle()
+        self.dr = DecoderRing(data='')
+
+        self.current_module = self.sc.instruction
+
+        self.modules = {
+            'summoning_circle': self.sc.instruction,
+            'sc': self.sc.instruction
+        }
 
     def _parser(self, command: str) -> list:
         """
@@ -24,19 +37,30 @@ class CyberBookInterpreter:
             return [split_command[0], split_command[1:]]
         return [split_command[0], None]
 
+    def _set_modules(self, module: str):
+        if module in self.modules.keys():
+            self.current_module = self.modules.get(module)
+            self.prompt: str = f'[CBI-{module.upper()}]-$ '
+            return f"Module set to '{module}'."
+        return f"Module '{module}' not found."
+
     def start_interpreter(self):
         try:
-            x = SummoningCircle()
             while True:
 
                 unprocessed_command = input(self.prompt)
                 if not unprocessed_command.strip():
-                    # empty command or no clients
+                    # or no clients
                     continue
+                elif 'exit' == unprocessed_command.lower():
+                    quit()
 
                 split_command = unprocessed_command.split()
 
-                print(x.instruction(*split_command))
+                if split_command[0] == '--module' and split_command[1].lower() in self.modules.keys():
+                    print(self._set_modules(module=split_command[1]))
+                else:
+                    print(self.current_module(*split_command))
 
         except KeyboardInterrupt:
             exit()
