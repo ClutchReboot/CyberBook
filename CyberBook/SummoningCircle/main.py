0461529ba246ad6d11bb1ed4a0086340f36c3558
ClutchTech <rollout26@gmail.com>
2022-12-07 12:05:51 -0500

    Refactored SummoningCircle and removed interpreter section.

--- CyberBook/SummoningCircle/main.py
+++ CyberBook/SummoningCircle/main.py
@@ -1,167 +1,107 @@
 import socket
-from select import select
 from threading import Thread
+from select import select
 
-from .listener import Server
 
-
-class SummoningCircle(Server):
+class SummoningCircle:
     """
-    NOTE: Currently, only works with SummoingCircleInterpreter. Separate package.
-    Interpreter used for SummoningCircleServer SummoningCircle.
+    Listener designed to work with Netcat reverse shells.
     """
-    def __init__(self):
+    def __init__(
+            self,
+            local_host: str = '0.0.0.0',
+            local_port: int = 5000,
+            timeout_in_sec: int = 5,
+            carriage_return: str = '\n'
+    ):
 
-        super().__init__()
+        self.local_host = local_host
+        self.local_port = local_port
+        self.timeout_in_sec = timeout_in_sec
+        self.carriage_return = carriage_return
 
-        self.command_prefix: str = '--'
-        self.buffer_size: int = 1024 * 25
-        self.carriage_return: str = '\n'
-        self.timeout_in_sec: int = 5
+        self.buffer_size = 1024 * 25
 
-        self._basic_functions = {
-            f"{self.command_prefix}shutdown": self.shutdown,
-            f"{self.command_prefix}exit": self.exit,
-            f"{self.command_prefix}view": self.view_clients
-        }
+        self.server: socket = None
+        self.clients: list[socket] = []
 
-        self._advanced_functions = {
-            f"{self.command_prefix}name": self.change_nickname,
-            f"{self.command_prefix}session": self.change_session
-        }
+        self.active_session: socket = None
 
-    def _parser(self, *args, **kwargs) -> list:
+    def _bind_socket(self) -> None:
         """
-        Parse used to set up decision-making.
-        """
-
-        args_list = list(args)
-        command = args_list.pop(0)
-
-        if len(args) > 1:
-            return [command, args_list]
-        return [command, None]
-
-    def _set_active_session(self):
-        """
-        Used to ensure 'self.active_session' is using the correct client socket.
-        """
-        if self.clients:
-            self.active_session = self.clients[self.active_client_index].get('client_socket')
-
-    def change_nickname(self, interpreter_options: list[int, str]):
-        """
-        Command: Change 'client_nickname' for a specified index in 'self.clients'
-        """
-        if len(interpreter_options) > 1:
-            index = int(interpreter_options[0])
-            name_change = interpreter_options[1]
-            self.clients[index]['client_nickname'] = name_change
-            return f'Name changed: {name_change}'
-
-    def change_session(self, interpreter_options: list[int]):
-        """
-        Command: Change client session
-        """
-
-        self.active_client_index = int(interpreter_options[0])
-        return f'Session changed: {self.active_client_index}'
-
-    def exit(self):
-        """
-        Command: Exit client connection and close connection.
-        Also, clean up 'self.clients' list.
+        Start listening socket server.
         """
         try:
-            # Create new list excluding current client_socket
-            self.clients = [
-                client for index, client in enumerate(self.clients) if self.active_session != client['client_socket']
-            ]
 
-            self.active_session.shutdown(socket.SHUT_RDWR)
-            self.active_session.close()
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind((self.local_host, self.local_port))
+            self.server.listen(5)
+
+            print(f"Listening for connection(s)...")
+
+            while True:
+                client_socket, client_address = self.server.accept()
+                self.clients.append(client_socket)
+
+                if not self.active_session:
+                    self.active_session = client_socket
+
+                print(f"Connection: {client_address[0]}:{client_address[1]}")
+
+        except (socket.error, KeyboardInterrupt):
+            return f'[-] Problem encountered: {socket.error} '
+
+    def end_client_session(self, client_session: socket = None):
+        """
+        Exit client connection and close connection.
+        Also, clean up 'self.clients' list.
+        """
+
+        if not client_session:
+            client_session = self.active_session
+
+        try:
+            self.clients.remove(client_session)
+            client_session.shutdown(socket.SHUT_RDWR)
+            client_session.close()
         except (socket.error, OSError, ValueError):
             pass
 
-    def is_active(self):
+    def send(self, command: str, client_session: socket = None) -> str:
         """
-        Command: Basic ping check to test if client is active.
-        Works for basic connections but not a viable solution for shell / SummoningCircle concept.
+        Default way to send OS commands to the active connection's shell.
         """
-        try:
-            self.active_session.send(b'echo ping\n')
-            return True
-        except socket.error:
-            return False
 
-    def send_command(self, command: str) -> str:
-        """
-        Command: Default way to send OS commands to the active connection's shell.
-        """
-        self.active_session.send(f"{command}{self.carriage_return}".encode())
+        if not client_session:
+            client_session = self.active_session
+
+        client_session.send(f"{command}{self.carriage_return}".encode())
 
         # Confirm data is being sent with a timeout
-        ready = select([self.active_session], [], [], self.timeout_in_sec)
+        ready = select([client_session], [], [], self.timeout_in_sec)
 
         if ready[0]:
-            return self.active_session.recv(self.buffer_size).decode()
+            return client_session.recv(self.buffer_size).decode()
 
         return 'Response timed Out.'
 
-    def shutdown(self):
+    def stop(self):
         """
-        Command: Close client connections and shutdown server.
-        Finally, system exit.
+        Close client connections and shutdown server.
         """
         try:
             for client in self.clients:
-                client['client_socket'].close()
+                client.close()
             self.server.shutdown(socket.SHUT_RDWR)
         except (socket.error, OSError, ValueError):
             pass
         self.server.close()
-        exit()
 
-    def start_listener(self):
+    def start(self):
         """
         Startup SummoningCircle in the background.
         Start interpreter.
         """
-        listener = Thread(target=self.bind_socket)
+        listener = Thread(target=self._bind_socket)
         listener.start()
         return "Server started"
-
-    def view_clients(self):
-        """
-        Command: View all client connections in 'self.clients'
-        """
-        output = '[*] Summoned Connections\n\n'
-        for index, client in enumerate(self.clients):
-            output += f"{index} -> {client['client_nickname']} : {client['client_address']}\n"
-        return output
-
-    def instruction(self, *args, **kwargs):
-        """
-        Used for decision making. Called by main interpreter function.
-        """
-
-        if 'start' == args[0].lower():
-            return self.start_listener()
-
-        if not self.clients:
-            # empty command or no clients
-            return "No clients"
-
-        self._set_active_session()
-
-        interpreter_command, interpreter_options = self._parser(*args, **kwargs)
-
-        if interpreter_command in self._basic_functions.keys():
-            function = self._basic_functions.get(interpreter_command)
-            print(function())
-        elif interpreter_command in self._advanced_functions.keys():
-            function = self._advanced_functions.get(interpreter_command)
-            print(function(interpreter_options))
-        else:
-            command = ' '. join(list(args))
-            print(self.send_command(command=command))
