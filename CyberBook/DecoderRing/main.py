8fce787a084379cdd37f1032afe7462795a94e79
Clutch_Reboot <39310845+ClutchReboot@users.noreply.github.com>
2023-02-07 13:27:22 -0500

    refactor and additions (#10)
    Co-authored-by: Clutch <clutchshadow26@gmail.com>

--- CyberBook/DecoderRing/main.py
+++ CyberBook/DecoderRing/main.py
@@ -1,119 +1,83 @@
 from base64 import urlsafe_b64decode, urlsafe_b64encode
-import hashlib
 from html import escape, unescape
-
 from . import utils
 
 
-class DecoderRing:
+import hashlib
+import urllib.parse
+
+
+def caeser(data, shift: int = 5, decode: bool = False) -> str:
     """
-    Many options for encoding / decoding.
-    Also includes some encryption options such as MD5.
+    Caeser's Cipher used on a string.
     """
-    def __init__(self, data: str = '') -> None:
-        self.data: str = data
-        self.altered_data: str = ''
 
-        self.shift: int = 0
+    if decode:
+        shift = -shift
 
-        if not isinstance(data, str):
-            raise TypeError("Input 'data' requires type 'str'.")
+    result = ""
 
-    def __call__(self):
-        return {
-            "data": self.data,
-            "altered_data": self.altered_data
-        }
+    for char in data:
+        if utils.is_not_ascii_letter(character=char):  # Account for spaces and special chars.
+            result += char
+        elif char.isupper():
+            enciphered_index = (utils.ascii_to_index(letter=char) + shift) % 26
+            result += utils.index_to_ascii(index=enciphered_index, capitalize=True)
+        else:
+            enciphered_index = (utils.ascii_to_index(letter=char) + shift) % 26
+            result += utils.index_to_ascii(index=enciphered_index)
+    return result
 
-    def _assign_data(self) -> str:
-        """
-        Use self.altered_data over self.data.
-        """
-        if self.altered_data:
-            return self.altered_data
-        return self.data
 
-    def set_data(self, data: str):
-        """
-        Setter function. To be used with interpreter.
-        """
-        self.data = data
-        return self.data
+def base64(data: str, decode: bool = False) -> str:
+    """
+    By default, encode into Base64 string.
+    """
 
-    def base64(self, decode: bool = False) -> str:
-        """
-        By default, encode into Base64 string.
-        """
+    if decode:
+        return urlsafe_b64decode(data).decode()
 
-        data = self._assign_data()
+    encoded = urlsafe_b64encode(data.encode('utf-8'))
+    return encoded.decode()
 
-        if decode:
-            decoded = urlsafe_b64decode(data)
-            self.altered_data = decoded.decode()
-            return self.altered_data
 
-        encoded = urlsafe_b64encode(data.encode('utf-8'))
-        self.altered_data = str(encoded, 'utf-8')
-        return self.altered_data
+def full_url(data: str, decode: bool = False) -> str:
 
-    def hex(self, decode: bool = False) -> str:
-        """
-        By default, encode into Hexadecimal string.
-        """
-        data = self._assign_data()
+    if decode:
+        url_decoded = data.replace('%2E', '.')
+        return urllib.parse.unquote(url_decoded)
 
-        if decode:
-            self.altered_data = bytes.fromhex(data).decode('utf-8')
-            return self.altered_data
+    url_encode = urllib.parse.quote(data)
+    return url_encode.replace('.', '%2E')
 
-        bytes_conversion = data.encode('utf-8')
-        self.altered_data = bytes_conversion.hex()
-        return self.altered_data
 
-    def html(self, decode: bool = False) -> str:
-        """
-        By default, encode into HTML safe string.
-        """
-        data = self._assign_data()
+def basic_hex(data, decode: bool = False) -> str:
+    """
+    By default, encode into Hexadecimal string.
+    """
+    if decode:
+        return bytes.fromhex(data).decode()
 
-        if decode:
-            self.altered_data = unescape(data)
-            return self.altered_data
+    bytes_conversion = data.encode()
+    return bytes_conversion.hex()
 
-        self.altered_data = escape(data)
-        return self.altered_data
 
-    def caeser(self, shift: int = 5, decode: bool = False) -> str:
-        """
-        Caeser's Cipher used on a string.
-        """
-        data = self._assign_data()
+def html(data, decode: bool = False) -> str:
+    """
+    By default, encode into HTML safe string.
+    """
+    if decode:
+        return unescape(data)
 
-        if self.shift:
-            shift = self.shift
+    return escape(data)
 
-        if decode:
-            shift = -shift
 
-        result = ""
+def md5(data):
+    """
+    Encrypt string into MD5 hash.
+    """
+    hashed_value = hashlib.md5(data.encode())
+    return hashed_value.hexdigest()
 
-        for char in data:
-            if utils.is_not_ascii_letter(character=char):  # Account for spaces and special chars.
-                result += char
-            elif char.isupper():
-                enciphered_index = (utils.ascii_to_index(letter=char) + shift) % 26
-                result += utils.index_to_ascii(index=enciphered_index, capitalize=True)
-            else:
-                enciphered_index = (utils.ascii_to_index(letter=char) + shift) % 26
-                result += utils.index_to_ascii(index=enciphered_index)
-        self.altered_data = result
-        return self.altered_data
 
-    def md5(self):
-        """
-        Encrypt string into MD5 hash.
-        """
-        data = self._assign_data()
-        hashed_value = hashlib.md5(data.encode())
-        self.altered_data = hashed_value.hexdigest()
-        return self.altered_data
+
--- CyberBook/test/test_DecoderRing.py
+++ CyberBook/test/test_DecoderRing.py
@@ -4,40 +4,30 @@ import unittest
 
 class DecoderRingTests(unittest.TestCase):
 
-    def test_invalid_data_input(self):
-        with self.assertRaises(TypeError):
-            DecoderRing(data=74657374)
-
     def test_successful_hex_encode(self):
-        dr = DecoderRing(data='test')
-        self.assertEqual(dr.hex(), '74657374')
+        dr = DecoderRing
+        self.assertEqual(dr.basic_hex('test'), '74657374')
 
     def test_successful_hex_decode(self):
-        dr = DecoderRing(data='74657374')
-        self.assertEqual(dr.hex(decode=True), 'test')
+        dr = DecoderRing
+        self.assertEqual(dr.basic_hex(data='74657374', decode=True), 'test')
 
     def test_successful_base64_encode(self):
-        dr = DecoderRing(data='test')
-        self.assertEqual(dr.base64(), 'dGVzdA==')
+        dr = DecoderRing
+        self.assertEqual(dr.base64(data='test'), 'dGVzdA==')
 
     def test_successful_base64_decode(self):
-        dr = DecoderRing(data='dGVzdA==')
-        self.assertEqual(dr.base64(decode=True), 'test')
+        dr = DecoderRing
+        self.assertEqual(dr.base64(data='dGVzdA==', decode=True), 'test')
 
     def test_successful_md5_encrypt(self):
-        dr = DecoderRing(data='test')
-        self.assertEqual(dr.md5(), '098f6bcd4621d373cade4e832627b4f6')
-
-    def test_chain_hex_base64_md5(self):
-        dr = DecoderRing(data='test')
-        dr.hex()
-        dr.base64()
-        self.assertEqual(dr.md5(), 'd3808ae5169407ecee946a6d3173b965')
+        dr = DecoderRing
+        self.assertEqual(dr.md5(data='test'), '098f6bcd4621d373cade4e832627b4f6')
 
     def test_chain_hex_base64_decode(self):
-        dr = DecoderRing(data='NzQ2NTczNzQ=')
-        dr.base64(decode=True)
-        self.assertEqual(dr.hex(decode=True), 'test')
+        dr = DecoderRing
+        self.assertEqual(dr.basic_hex(
+            data=dr.base64(data='NzQ2NTczNzQ=', decode=True), decode=True), 'test')
 
 
 if __name__ == '__main__':
