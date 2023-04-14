7fd2a24ea1cd419599aa69301766cd6404dde228
Clutch_Reboot <39310845+ClutchReboot@users.noreply.github.com>
2023-04-14 17:38:49 -0400

    Update os_specifics.py

--- CyberBook/Identify/os_specifics.py
+++ CyberBook/Identify/os_specifics.py
@@ -1,20 +1,35 @@
 import socket
 import platform
-import datetime
 
 
+def get_private_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('1.1.1.2', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+
+    finally:
+        s.close()
+    
+    return ip
+    
+    
 def os_specs():
     hostname = socket.gethostname()
+
     return {
         "hostname": hostname,
         "hostByHostname": socket.gethostbyname(hostname),
         "hostByAddress": list(socket.gethostbyname_ex(hostname)),
+        "privateIp": get_private_ip(),
         "platform": {
             "system": platform.system(),
             "version": platform.version(),
             "platform": platform.platform(),
             "machine": platform.machine(),
             "processor": platform.processor()
-        },
-        "latestUpdate": datetime.datetime.now().strftime('%H%M-%d%m%Y')
+        }
     }
